01182a116372cf0ec0d7f4ac4494edac60a0de65
dhanraj shetty <dhanrajcoding@gmail.com>
2025-08-13 23:40:51 +0530

    intialized starter files and added a button

---
+++ framer_motion/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+<div className="h-screen w-full flex items-center justify-center bg-neutral-900">
+  <button className='text-white py-2 px-6 bg-blue-700 rounded-full'>subscribe</button>
+</div>
+  )
+}
+
+export default App
